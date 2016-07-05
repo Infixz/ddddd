7d46948b468e9c96d71273e4da649bb5ac1d3f5d
jonathan <infixz520@gmail.com>
2016-07-05 22:21:06 +0800

    tiny

--- server.py
+++ server.py
@@ -11,79 +11,79 @@ from config import WX_PLAT_TOKEN, SETTINGS, DB_HOST, DB_PORT
 from util import check_signature,parse
 
 class IndexHandler(web.RequestHandler):
-	"""docstring for IndexHandler"""
-	def get(self):
-		# greetingwords = self.get_argument('greeting','hello')
-		# self.write(greetingwords + u',再次见面分外眼红')
-		self.render('index.html')
+    """docstring for IndexHandler"""
+    def get(self):
+        # greetingwords = self.get_argument('greeting','hello')
+        # self.write(greetingwords + u',再次见面分外眼红')
+        self.render('index.html')
 
 class ResumeHandler(web.RequestHandler):
-	"""docstring for ResumeHandler"""
-	def get(self):
-		self.render('resume.html')
+    """docstring for ResumeHandler"""
+    def get(self):
+        self.render('resume.html')
 
 
 class LeavMsgHandler(web.RequestHandler):
-	"""docstring for MsgHandler"""
-	def post(self):
-		print 'MsgHandler post'
-		try:
-			self.coll = self.application.db.leavemsg
-		except:
-			print 'except'
+    """docstring for MsgHandler"""
+    def post(self):
+        print 'MsgHandler post'
+        try:
+            self.coll = self.application.db.leavemsg
+        except:
+            print 'except'
 
 class GoogleVerifyHandler(web.RequestHandler):
-	def get(self):
-		self.render('google05df3613559d1aa7.html')
+    def get(self):
+        self.render('google05df3613559d1aa7.html')
 
 class WxHandler(web.RequestHandler):
-	# TODO:
-	# Add async post method
-	"""handler for ddddd on weixin_platform"""
-	def get(self):
-		"""check token"""
-		print 'check wx'
-		if self._check_signature():
-			self.write(self.get_query_argument('echostr'))
-		else:
-			self.write('Naughty boy~')
-			
-	def post(self):
-		"""response weixin msg"""
-		if not self._check_signature():
-			self.write('Naughty boy~')
-		resp = parse(self.request.body)
-		print 'POSTed a msg'
-		self.write(resp)
-		
-	def _check_signature(self):
-		try:
-			nonce = self.get_query_argument('nonce')
-			signature = self.get_query_argument('signature')
-			timestamp = self.get_query_argument('timestamp')
-		except Exception as exception:
- 			print 'Checked False'
-			return False
-		else:
-			return check_signature(signature, timestamp, nonce)
+    # TODO:
+    # Add async post method
+    """handler for ddddd on weixin_platform"""
+    def get(self):
+        """check token"""
+        print 'check wx'
+        if self._check_signature():
+            self.write(self.get_query_argument('echostr'))
+        else:
+            self.write('Naughty boy~')
+            
+    def post(self):
+        """response weixin msg"""
+        if not self._check_signature():
+            self.write('Naughty boy~')
+        resp = parse(self.request.body)
+        print 'POSTed a msg'
+        self.write(resp)
+        
+    def _check_signature(self):
+        try:
+            nonce = self.get_query_argument('nonce')
+            signature = self.get_query_argument('signature')
+            timestamp = self.get_query_argument('timestamp')
+        except Exception as exception:
+            print 'Checked False'
+            return False
+        else:
+            return check_signature(signature, timestamp, nonce)
 
 
 class App(web.Application):
-	"""docstring for App"""
-	def __init__(self):
-		handlers = [(r'/',IndexHandler),
-					(r'/resume',ResumeHandler),
-					(r'/leavemsg',LeavMsgHandler),
-					(r'/weixin',WxHandler),
-					(r'/google05df3613559d1aa7.html',GoogleVerifyHandler)
-					]
-		self.db = MongoClient(DB_HOST,DB_PORT)['usertest']
-		web.Application.__init__(self,handlers,**SETTINGS)
+    """docstring for App"""
+    def __init__(self):
+        handlers = [(r'/',IndexHandler),
+                    (r'/resume',ResumeHandler),
+                    (r'/leavemsg',LeavMsgHandler),
+                    (r'/weixin',WxHandler),
+                    (r'/google05df3613559d1aa7.html',GoogleVerifyHandler)
+                    ]
+        self.db = MongoClient(DB_HOST,DB_PORT)['usertest']
+        web.Application.__init__(self,handlers,**SETTINGS)
 
 
 
 if __name__ == '__main__':
-	http_server = httpserver.HTTPServer(App())
-	print 'App has being run on server'
-	http_server.listen(8880)
-	ioloop.IOLoop.instance().start()
+    http_server = httpserver.HTTPServer(App())
+    print 'App has being run on server'
+    http_server.listen(8880)
+    ioloop.IOLoop.instance().start()
